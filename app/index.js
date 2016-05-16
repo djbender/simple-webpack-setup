20eecbe0647563ecb9fc4a20d13c5c7cdea777fa
Spencer Olson <solson@instructure.com>
2016-05-15 22:30:27 -0400

    add babel-preset-react-hmre

--- app/index.js
+++ app/index.js
@@ -1,4 +1,7 @@
-var component = require('./component');
-require('./main.css');
+import './main.css';
 
-document.body.appendChild(component());
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
